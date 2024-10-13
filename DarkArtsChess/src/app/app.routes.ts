51b050acd23e176bd232ec012354c8f8f28a52a2
Emmaoshin <ealmont4@student.ccp.edu>
2024-10-13 17:46:23 -0400

    Fixed routing to deal with non common routes

--- DarkArtsChess/src/app/app.routes.ts
+++ DarkArtsChess/src/app/app.routes.ts
@@ -1,27 +1,29 @@
 import { Routes } from '@angular/router';
 import { ChessboardComponent } from './Components/chess-board/chess-board.component';
-import {  LoginComponent } from './Components/login/login.component';
+import { LoginComponent } from './Components/login/login.component';
 import { AuthGuard } from './auth.guard';
 import { AppComponent } from './app.component';
 // add routes for chessboard component
 export const routes: Routes = [
-  // {
-  //   path: "",
-  //   component: AppComponent,
-  //   canActivate: [AuthGuard],
-  // },
   {
-    path: "chessboard",
-    component: ChessboardComponent,
-    data: {
-      "title": "Chessboard"
-    }
+    path: '',
+    component: AppComponent,
+    // canActivate: [AuthGuard],
   },
   {
-    path: "login",
+    path: 'chessboard',
+    component: ChessboardComponent,
+    data: {
+      title: 'Chessboard',
+    },
+  },
+  {
+    path: 'login',
     component: LoginComponent,
     data: {
-      "title": "Login"
-    }
-  }
+      title: 'Login',
+    },
+  },
+  // { path: '', component: HomeComponent },
+  { path: '**', component: ChessboardComponent }, // This catches all undefined routes
 ];
